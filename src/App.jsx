414161c3173c3f8889fd437e6c0cd7e829f4e1b6
Kehinde Olaleye Simeon <simeonkenny66@gmail.com>
2024-02-13 12:13:46 +0100

    react styling and interpolation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import simeonStyle from './simeon.module.css'
 
 const App = () => {
+  let myAge = 100;
+  let myStyle = { color: "", backgroundColor: "grey" };
   return (
     <>
-    <Navbar></Navbar>
+    <h2 className={simeonStyle.myHeader}>Jesus my Lover</h2>
+      <h3 style={myStyle}>{myAge}</h3>
+      <h2 style={{ color: "", backgroundColor: "blue" }}>I love Jesus</h2>
+      <Navbar></Navbar>
     </>
   );
 };
