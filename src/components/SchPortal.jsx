49ee27f403f46bfa25a6c0e6c98f4c08b44ad4f8
Kehinde Olaleye Simeon <simeonkenny66@gmail.com>
2024-04-04 12:07:50 +0100

    relearning mapping

--- src/components/SchPortal.jsx
+++ src/components/SchPortal.jsx
@@ -6,46 +6,53 @@ const SchPortal = () => {
   const [lastname, setlastname] = useState("");
   const [email, setemail] = useState("");
   const [password, setpassword] = useState("");
+  const [allStudents, setallStudents] = useState([]);
 
-  const register = () => {};
-  const changeFirstName = (e) => {
-    setfirstname(e.target.value)
-};
-  const changeLastName = (e) => {
-    setlastname(e.target.value)
-};
+  const register = () => {
+    let student = { firstname, lastname, email, password };
+    console.log(student);
+    setallStudents([...allStudents, student]);
+  };
+
   return (
     <>
       <div className="d-grid align-items-center justify-content-center p-5">
-        <h1 className="display-1 ">Simeon Group of Schools</h1>
+        <h1 className="display-1 ">Smion Group of Schools</h1>
         <input
           type="text"
           className="form-control form-control-lg m-2"
           placeholder="FirstName"
-          onChange={(e) => changeFirstName(e)}
+          onChange={(e) => setfirstname(e.target.value)}
         />
         <input
           type="text"
           className="form-control form-control-lg m-2"
           placeholder="LastName"
-          onChange={(e) => changeLastName(e)}
+          onChange={(e) => setlastname(e.target.value)}
         />
         <input
           type="email"
           className="form-control form-control-lg m-2"
           placeholder="Email"
+          onChange={(e) => setemail(e.target.value)}
         />
         <input
           type="password"
           className="form-control form-control-lg m-2"
           placeholder="Password"
+          onChange={(e) => setpassword(e.target.value)}
         />
         <button className="btn btn-lg btn-dark " onClick={register}>
           Submit
         </button>
         <hr />
-        <hr />
-        <div>{firstname} {lastname}</div>
+        {allStudents.map((student, index) => (
+          <div key={index} className="">
+            <h1>{student.firstname}</h1>
+            <button>Edit</button>
+            <button>Delete</button>
+          </div>
+        ))}
       </div>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import './mine.css'
+// import './mine.css'
 import App from './App.jsx'
 import UstateOne from './components/UstateOne.jsx'
 import UstateTwo from './components/UstateTwo.jsx'
